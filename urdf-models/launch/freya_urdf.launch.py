1fae919b2967a3ce51e09974842a9e050594a67c
Eirik Kolås <kolaseirik@gmail.com>
2024-03-29 14:34:16 +0100

    added launch file to launch urdf model and thruster viz

--- urdf-models/launch/freya_urdf.launch.py
+++ urdf-models/launch/freya_urdf.launch.py
@@ -7,29 +7,20 @@ from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    ld = LaunchDescription()
-    package_name = 'urdf_models'
-    urdf_default_path = PathJoinSubstitution(['urdf', 'freya', 'ASV_MainAssembly.urdf'])
+    urdf_path = PathJoinSubstitution(['urdf', 'freya', 'ASV_MainAssembly.urdf'])
 
-    # Declare the launch arguments (for this launch file)
-    urdf_package_path_launch_arg = DeclareLaunchArgument(
-        name='model',
-        default_value=urdf_default_path, 
-        description='Path to robot urdf file relative to package'
-    )
-
-    # Include the launch description for the urdf launch file
+    # Include the launch description for the urdf launch file (separate package. Find it using rosdep)
     urdf_launch_path = PathJoinSubstitution([FindPackageShare('urdf_launch'), 'launch', 'description.launch.py'])
     urdf_launch = IncludeLaunchDescription(
         launch_description_source=urdf_launch_path,
         launch_arguments={
-            'urdf_package': package_name,
-            'urdf_package_path': LaunchConfiguration('model')
+            'urdf_package': 'urdf_models',
+            'urdf_package_path': urdf_path
         }.items()
     )
 
-    # base_link (NED) to base_link (SEU) tf. TODO: Move to asv_setup tf.launch.py
-    tf_base_link_ned_to_base_link_enu = Node(
+    # base_link (NED) to freya_model
+    tf_base_link_to_freya_model = Node(
         package='tf2_ros',
         executable='static_transform_publisher',
         arguments=['--x'             , '0',
@@ -39,11 +30,10 @@ def generate_launch_description():
                    '--pitch'         , str(math.pi),
                    '--yaw'           , '0',
                    '--frame-id'      , 'base_link',
-                   '--child-frame-id', 'base_link_SEU']
+                   '--child-frame-id', 'freya_model']
     )
 
     return LaunchDescription([
-        urdf_package_path_launch_arg,
         urdf_launch,
-        tf_base_link_ned_to_base_link_enu
+        tf_base_link_to_freya_model
     ])
